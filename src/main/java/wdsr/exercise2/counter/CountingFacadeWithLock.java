c5fa39d867ae40630b8d6858fca99583ed36b1a8
kozirro <kubaton1994@o2.pl>
2016-04-01 18:08:33 +0200

    Update CountingFacadeWithLock.java

--- src/main/java/wdsr/exercise2/counter/CountingFacadeWithLock.java
+++ src/main/java/wdsr/exercise2/counter/CountingFacadeWithLock.java
@@ -1,5 +1,8 @@
 package wdsr.exercise2.counter;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Marek on 05.03.2016.
  * 
@@ -9,17 +12,29 @@ public class CountingFacadeWithLock implements CountingFacade {
 	private final BusinessService businessService;
 	
 	private int invocationCounter;
+	private final Lock lock = new ReentrantLock();
 	
 	public CountingFacadeWithLock(BusinessService businessService) {
 		this.businessService = businessService;
 	}
 		
 	public void countAndInvoke() {
-		invocationCounter++;
+		lock.lock();
+		try{
+			invocationCounter++;
+		} finally { 
+			lock.unlock();
+		}
 		businessService.executeAction();
 	}
 	
 	public int getCount() {
 		return invocationCounter;
 	}
+	
+	
+	
+	
+	
+	
 }
