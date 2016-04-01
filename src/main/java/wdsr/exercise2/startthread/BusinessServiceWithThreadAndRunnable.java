db8eea8042b0d9951e5d1f29b10471187f3b4bcd
kozirro <kubaton1994@o2.pl>
2016-04-01 18:04:09 +0200

    Update BusinessServiceWithThreadAndRunnable.java

--- src/main/java/wdsr/exercise2/startthread/BusinessServiceWithThreadAndRunnable.java
+++ src/main/java/wdsr/exercise2/startthread/BusinessServiceWithThreadAndRunnable.java
@@ -15,7 +15,23 @@ public class BusinessServiceWithThreadAndRunnable {
 	 */
 	public void computeFibonacci(int n, FibonacciCallback callback) {
 		// TODO Task: execute the logic below in a new Thread, use Runnable interface.
-		long value = helper.findFibonacciValue(n);
-		callback.fibonacciComputed(value);
+		
+		
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				long value = helper.findFibonacciValue(n);
+				callback.fibonacciComputed(value);
+
+				
+			}
+		});
+		
+		thread.start();
+		
+		
+	
 	}
 }
