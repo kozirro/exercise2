9619b77c644b2e9f9157db4449650fc11ba0cf39
kozirro <kubaton1994@o2.pl>
2016-04-01 18:11:13 +0200

    Update BufferQueueImpl.java

--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,25 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Task: implement Exchange interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+
+	private BlockingQueue<Order> queue = new ArrayBlockingQueue<>(20000);
+	
+	
+	public void submitOrder(Order order) throws InterruptedException {	
+		queue.put(order);
+		 	
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		
+		return queue.take();
 	}
+
+	
 }
